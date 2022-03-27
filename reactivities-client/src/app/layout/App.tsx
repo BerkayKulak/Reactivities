57555804dc97197873cb4abc33964657183e13a9
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-03-27 18:03:59 +0300

    Adding an Activity interface

--- reactivities-client/src/app/layout/App.tsx
+++ reactivities-client/src/app/layout/App.tsx
@@ -1,22 +1,25 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Header, List } from "semantic-ui-react";
+import { Activity } from "../models/activity";
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios.get("https://localhost:44335/api/activities").then((response) => {
-      setActivities(response.data);
-    });
+    axios
+      .get<Activity[]>("https://localhost:44335/api/activities")
+      .then((response) => {
+        setActivities(response.data);
+      });
   }, []);
 
   return (
-    <div >
+    <div>
       <Header as="h2" icon="users" content="Reactivities" />
 
       <List>
-        {activities.map((activity: any) => (
+        {activities.map((activity) => (
           <List.Item key={activity.id}>{activity.title}</List.Item>
         ))}
       </List>
