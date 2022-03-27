95a4f29bfaab821c10ec272db7f9b2773c553a53
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-03-27 15:59:12 +0300

    Creating our first Query handler

--- Reactivities.API/Controllers/ActivitiesController.cs
+++ Reactivities.API/Controllers/ActivitiesController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Reactivities.Application.Activities;
 using Reactivities.Domain;
 using Reactivities.Persistence;
 
@@ -12,23 +14,24 @@ namespace Reactivities.API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
+        private readonly IMediator _mediator;
 
-        public ActivitiesController(DataContext context)
+
+        public ActivitiesController(IMediator mediator)
         {
-            _context = context;
+            _mediator = mediator;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _context.Activities.ToListAsync();
+            return await _mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await _context.Activities.FindAsync(id);
+            return Ok();
         }
     }
 }
