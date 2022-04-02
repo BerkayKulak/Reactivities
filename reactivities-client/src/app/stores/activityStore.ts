96b0607f2571e5165d42c0cb5a1dd7d8bd32ea44
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-04-02 18:13:35 +0300

    Validation in Formik

--- reactivities-client/src/app/stores/activityStore.ts
+++ reactivities-client/src/app/stores/activityStore.ts
@@ -7,7 +7,7 @@ export default class ActivityStore {
   selectedActivity: Activity | undefined = undefined;
   editMode = false;
   loading = false;
-  loadingInitial = true;
+  loadingInitial = false;
 
   constructor() {
     makeAutoObservable(this);
@@ -23,7 +23,9 @@ export default class ActivityStore {
     return Object.entries(
       this.activitiesByDate.reduce((activities, activity) => {
         const date = activity.date;
-        activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+        activities[date] = activities[date]
+          ? [...activities[date], activity]
+          : [activity];
         return activities;
       }, {} as { [key: string]: Activity[] })
     );
