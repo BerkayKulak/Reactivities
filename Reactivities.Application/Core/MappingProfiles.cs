866e4bc9dbcdd70ccfb9c62e097c38f546026f14
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-04-07 13:06:37 +0300

    Shaping the related data

--- Reactivities.Application/Core/MappingProfiles.cs
+++ Reactivities.Application/Core/MappingProfiles.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using Reactivities.Application.Activities;
 using Reactivities.Domain;
 
@@ -14,6 +9,7 @@ namespace Reactivities.Application.Core
         public MappingProfiles()
         {
             CreateMap<Activity, Activity>();
+            CreateMap<Activity, ActivityDto>();
         }
     }
 }
