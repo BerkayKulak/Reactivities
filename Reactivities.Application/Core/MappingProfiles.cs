28abb011906432a0463de702622c930b6e495b19
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-04-10 11:11:44 +0300

    Adding the following property to the mapping configuration

--- Reactivities.Application/Core/MappingProfiles.cs
+++ Reactivities.Application/Core/MappingProfiles.cs
@@ -10,6 +10,8 @@ namespace Reactivities.Application.Core
     {
         public MappingProfiles()
         {
+            string currentUsername = null;
+
             CreateMap<Activity, Activity>();
             CreateMap<Activity, ActivityDto>().
                 ForMember(d => d.HostUsername, o => o.
@@ -27,7 +29,8 @@ namespace Reactivities.Application.Core
                 ForMember(d => d.Image,
                 o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url)).
                 ForMember(d => d.FollowersCount, o => o.MapFrom(s => s.Followers.Count)).
-                ForMember(d => d.FollowingCount, o => o.MapFrom(s => s.Followings.Count));
+                ForMember(d => d.FollowingCount, o => o.MapFrom(s => s.Followings.Count)).
+                ForMember(d => d.Following, o => o.MapFrom(s => s.Followers.Any(x => x.Observer.UserName == currentUsername)));
 
             CreateMap<Comment, CommentDto>()
                 .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.Author.DisplayName))
